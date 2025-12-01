c0f9b2d9a0aa38e00f12fb09162f47b2a01ed751
SourabhSourabh <105868856+SourabhSourabh@users.noreply.github.com>
2025-12-01 22:28:07 +0530

    Add solution for Find the Duplicate Number

---
+++ solutions/medium/Find the Duplicate Number.java
@@ -0,0 +1,17 @@
+// Title: Find the Duplicate Number
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-duplicate-number/
+
+            else{
+                nums[trial]= -nums[trial];
+            }
+            }
+
+
+        }
+        return -1;
+
+    }
+}
+                return trial;
