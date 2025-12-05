dcaeca76b5a09747df3a10643662f6e4991cb200
SourabhSourabh <105868856+SourabhSourabh@users.noreply.github.com>
2025-12-05 22:27:10 +0530

    Add solution for Merge Intervals

---
+++ solutions/medium/Merge Intervals.java
@@ -0,0 +1,18 @@
+// Title: Merge Intervals
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/merge-intervals/
+
+
+        }
+            list.add(i);
+
+          ArrayList<int[]>list = new ArrayList<>();
+        for(int i [] : intervals){
+        }
+            return intervals;
+        if(intervals.length==1){
+        
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
