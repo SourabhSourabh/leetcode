5f2348e1b41929c2e3adbd44d8ac1bae5beffcc4
SourabhSourabh <105868856+SourabhSourabh@users.noreply.github.com>
2026-01-02 17:55:19 +0530

    Time: 1 ms (99.55%) | Memory: 83 MB (43.33%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,6 +1,20 @@
 class Solution {
+
+    static
+    
+    {
+
+        Solution obj = new Solution();
+        for(int i=0;i<500;i++){
+            obj.findDuplicate(null);
+        }
+
+
+    }
     public int findDuplicate(int[] nums) {
         
+
+        if(nums==null)return -1;
 
        for(int i : nums){
 
